f22d5fcaedaf8ad2b9c031e4abc5f953d278d0f8
GeumBinLee <lgb9811@gmail.com>
2023-03-21 13:56:53 +0900

    win32com으로 모든 데이터 뽑기

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
-import pymysql
+import pymysql, os
+from . import extract_data
 
 db = pymysql.connect(
     user='root',
@@ -8,9 +9,21 @@ db = pymysql.connect(
 
 cursor = db.cursor(pymysql.cursors.Cursor)
 
-sql = "select * from new_table"
 
-cursor.execute(query = sql)
+# df = pd.read_excel(f"{os.getcwd()}\일괄등록테스트.xlsx")
+# Data = []
+# for i in range(1, len(df.columns)-3) :
+#     Data.append(df.iloc[i].to_json())
 
-result = cursor.fetchall()
-print(result)
+
+
+sql = "insert into new_table values(NULL, '데모 브랜드', 'sku_101882090001', 'kingsman', 'sku_101882090001', '부제', '골든서클', 29000, 4387, '과세', '20세기스튜디오', '영화', '액션', 'https://pupple-images.s3.ap-northeast-2.amazonaws.com/product/_demo-202201061953511641498831-스크린샷 2022-01-07 오전 4.53.22.png', 33, '010-0000-0000', 'BULK_MIG', NULL)"
+
+
+
+# sql = "select * from new_table"
+
+# cursor.execute(query = sql)
+
+# result = cursor.fetchall()
+# print(result)
--- extract_data.py
+++ extract_data.py
@@ -1,12 +1,27 @@
 from PIL import ImageGrab
 import win32com.client as win32
 import os
+
+
+
 excel = win32.gencache.EnsureDispatch('Excel.Application')
 curr = os.getcwd() + "/일괄등록테스트.xlsx"
 workbook = excel.Workbooks.Open(curr)
 
 
 
+# 이미지 아닌 데이터들 뽑기
+for sheet in workbook.Worksheets:
+    used_range = sheet.UsedRange
+    for r in range(used_range.Row + 1, used_range.Row + used_range.Rows.Count):
+        for c in range(used_range.Column, used_range.Column + used_range.Columns.Count):
+            cell = sheet.Cells(r, c)
+            print(cell.Value, cell.Address)
+# workbook.Close()
+
+
+
+# 이미지 추출 및 저장
 for sheet in workbook.Worksheets:
     for i, shape in enumerate(sheet.Shapes):
         if shape.Name.startswith('Picture'):  
@@ -19,6 +34,7 @@ for sheet in workbook.Worksheets:
             image=image.convert("RGB")
             image.save(f"pictures/{workbook_name}_{sheet_name}_{cell_position}.jpg", 'jpeg')
             print(f"Image {i+1} is in cell {cell_position}")
+    workbook.Close()
 
 
 
