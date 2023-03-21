04d45780abee5df9413e0724dcd0d9bf814f679b
GeumBinLee <lgb9811@gmail.com>
2023-03-21 15:08:47 +0900

    모듈화

--- db.py
+++ db.py
@@ -1,5 +1,8 @@
-import pymysql, os
-from . import extract_data
+import pymysql
+import os
+import extract_data
+
+curr = os.getcwd() + "/일괄등록테스트.xlsx"
 
 db = pymysql.connect(
     user='root',
@@ -9,21 +12,16 @@ db = pymysql.connect(
 
 cursor = db.cursor(pymysql.cursors.Cursor)
 
+results = extract_data.getExcelData(curr)
+print(results)
 
-# df = pd.read_excel(f"{os.getcwd()}\일괄등록테스트.xlsx")
-# Data = []
-# for i in range(1, len(df.columns)-3) :
-#     Data.append(df.iloc[i].to_json())
+for result in results:
+    sql = f"INSERT INTO new_table VALUES (NULL, '{result['BRAND_NM']}', '{result['PRIMARY_PROD_ID']}', '{result['PROD_ID']}', '{result['OPT_ID']}', '{result['OPT_NM_1']}', '{result['OPT_VALUE_1']}', '{result['PROD_PRICE']}', '{result['PROD_COST']}', '{result['TAX_CLF']}', '{result['PUPPLE_CATE_L']}', '{result['PUPPLE_CATE_M']}', '{result['PUPPLE_CATE_S']}', '{result['PROD_IMG_URL']}', '{result['PROD_INFO_ID']}', '{result['AS_PHONE_NUM']}', '{result['DELIV_POLICY_NM']}', '{result['img_path']}' )"
+    cursor.execute(query=sql)
+
+db.commit()
+db.close()
 
 
-
-sql = "insert into new_table values(NULL, '데모 브랜드', 'sku_101882090001', 'kingsman', 'sku_101882090001', '부제', '골든서클', 29000, 4387, '과세', '20세기스튜디오', '영화', '액션', 'https://pupple-images.s3.ap-northeast-2.amazonaws.com/product/_demo-202201061953511641498831-스크린샷 2022-01-07 오전 4.53.22.png', 33, '010-0000-0000', 'BULK_MIG', NULL)"
-
-
-
-# sql = "select * from new_table"
-
-# cursor.execute(query = sql)
-
-# result = cursor.fetchall()
-# print(result)
+pang = cursor.fetchall()
+print(pang)
--- extract_data.py
+++ extract_data.py
@@ -1,53 +1,39 @@
 from PIL import ImageGrab
 import win32com.client as win32
-import os
 
 
+def getExcelData(path) :
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    workbook = excel.Workbooks.Open(path)
 
-excel = win32.gencache.EnsureDispatch('Excel.Application')
-curr = os.getcwd() + "/일괄등록테스트.xlsx"
-workbook = excel.Workbooks.Open(curr)
+    # 셀마다 dict 만들기
+    cell_dicts = []
+    for sheet in workbook.Worksheets:
+        used_range = sheet.UsedRange
+        for r in range(used_range.Row + 1, used_range.Row + used_range.Rows.Count):
+            cell_dict = {"img_path": None}
+            for c in range(used_range.Column, used_range.Column + used_range.Columns.Count):
+                cell = sheet.Cells(r, c)
+                key = sheet.Cells(1, c).Value  # Use the first row as the key
+                if "Image" not in key:
+                    value = cell.Value
+                    cell_dict[key] = value
+            cell_dicts.append(cell_dict)
 
+    # 이미지 추출 및 저장
+    for sheet in workbook.Worksheets:
+        for i, shape in enumerate(sheet.Shapes):
+            if shape.Name.startswith('Picture'):  
+                workbook_name = workbook.Name.split(".")[0]
+                sheet_name = sheet.Name
+                cell_position = shape.TopLeftCell.Address
+                shape.Copy()
+                image = ImageGrab.grabclipboard()
+                image=image.convert("RGB")
+                image.save(f"pictures/{workbook_name}_{sheet_name}_{cell_position}.jpg", 'jpeg')
+                # 이미지 경로 삽입
+                cell_dict = cell_dicts[shape.TopLeftCell.Row - 2]  # 인덱스는 0부터 시작하고 첫 번째 행 제외해야 하니까 -2 해주기기
+                cell_dict["img_path"] = f"pictures/{workbook_name}_{sheet_name}_{cell_position}.jpg"
 
-
-# 이미지 아닌 데이터들 뽑기
-for sheet in workbook.Worksheets:
-    used_range = sheet.UsedRange
-    for r in range(used_range.Row + 1, used_range.Row + used_range.Rows.Count):
-        for c in range(used_range.Column, used_range.Column + used_range.Columns.Count):
-            cell = sheet.Cells(r, c)
-            print(cell.Value, cell.Address)
-# workbook.Close()
-
-
-
-# 이미지 추출 및 저장
-for sheet in workbook.Worksheets:
-    for i, shape in enumerate(sheet.Shapes):
-        if shape.Name.startswith('Picture'):  
-            workbook_name = workbook.Name.split(".")[0]
-            sheet_name = sheet.Name
-            cell_position = shape.TopLeftCell.Address
-            shape.Copy()
-            image = ImageGrab.grabclipboard()
-            print(image)
-            image=image.convert("RGB")
-            image.save(f"pictures/{workbook_name}_{sheet_name}_{cell_position}.jpg", 'jpeg')
-            print(f"Image {i+1} is in cell {cell_position}")
     workbook.Close()
-
-
-
-# 참고 코드
-# from PIL import ImageGrab
-# import win32com.client as win32
-
-# excel = win32.gencache.EnsureDispatch('Excel.Application')
-# workbook = excel.Workbooks.Open(r'C:\Users\file.xlsx')
-
-# for sheet in workbook.Worksheets:
-#     for i, shape in enumerate(sheet.Shapes):
-#         if shape.Name.startswith('Picture'):  # or try 'Image'
-#             shape.Copy()
-#             image = ImageGrab.grabclipboard()
-#             image.save('{}.jpg'.format(i+1), 'jpeg')
+    return cell_dicts
